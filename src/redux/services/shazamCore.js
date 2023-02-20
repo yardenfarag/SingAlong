6e95b0a500d5ff2cd1b911acdbfe74436e35f0de
Yarden Farag <yarden.farag@gmail.com>
2023-02-20 15:46:26 +0200

    added top play cmp and song details

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,19 +1,32 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const shazamCoreApi = createApi({
-    reducerPath: 'app/shazamCoreApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
-        prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', '35f41f862fmsh0405d02fe4fc189p100c45jsn3202ac3e7654')
-            return headers
-        }
-    }),
-    endpoints: (builder) => ({
-        getTopCharts: builder.query({query: () => '/charts/world'})
-    })
-})
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
+    getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
+    getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+    getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
+    getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
+  }),
+});
 
 export const {
-    useGetTopChartsQuery,
-} = shazamCoreApi
+  useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
+  useGetArtistDetailsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} = shazamCoreApi;
